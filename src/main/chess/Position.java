876954d7492d9854c0dc77d7f5e516ca5d9765d2
Spencer Neu <stneu73@gmail.com>
2023-10-06 14:09:49 -0600

    setup(created classes for each of the interfaces)

---
+++ src/main/chess/Position.java
@@ -0,0 +1,13 @@
+package chess;
+
+public class Position implements ChessPosition{
+    @Override
+    public int getRow() {
+        return 0;
+    }
+
+    @Override
+    public int getColumn() {
+        return 0;
+    }
+}
