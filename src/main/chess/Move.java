2908a18414c7e9234374f4e090174bf072f475fb
Spencer Neu <stneu73@gmail.com>
2023-10-06 17:37:22 -0600

    Instantiated Move class variables and method returns, started rook movement logic

--- src/main/chess/Move.java
+++ src/main/chess/Move.java
@@ -1,18 +1,30 @@
 package chess;
 
 public class Move implements ChessMove{
+    ChessPosition startPosition;
+    ChessPosition endPosition;
+    ChessPiece.PieceType promotionPiece = null;
+    public Move(ChessPosition startPosition, ChessPosition endPosition) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+    }
+    public Move(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
+    }
     @Override
     public ChessPosition getStartPosition() {
-        return null;
+        return this.startPosition;
     }
 
     @Override
     public ChessPosition getEndPosition() {
-        return null;
+        return this.endPosition;
     }
 
     @Override
     public ChessPiece.PieceType getPromotionPiece() {
-        return null;
+        return promotionPiece;
     }
 }
--- src/main/chess/Rook.java
+++ src/main/chess/Rook.java
@@ -20,6 +20,40 @@ public class Rook implements ChessPiece{
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        int x = myPosition.getColumn();
+        int y = myPosition.getRow();
+        boolean hitAnotherPiece = false;
+        //left
+        for (int i = x; i > 0; i--) {
+            if (hitAnotherPiece) break;
+            ChessPosition pos = new Position(y,i);
+            if (board.getPiece(pos) == null){
+                moves.add(new Move(myPosition,new Position(y,i)));
+            }
+            else if (board.getPiece(pos) != null) {
+                if (board.getPiece(pos).getTeamColor() == this.color) {
+                    hitAnotherPiece = true;
+                    break;
+                }
+                if (board.getPiece(pos).getTeamColor() != this.color) {
+                    moves.add(new Move(myPosition,new Position(y,i)));
+                    hitAnotherPiece = true;
+                    break;
+                }
+            }
+        }
+        //right
+        for (int i = x; i < 8; i++) {
+            moves.add(new Move(myPosition,new Position(y,i)));
+        }
+        //up
+        for (int i = y; i > 0; i--) {
+            moves.add(new Move(myPosition,new Position(i,x)));
+        }
+        //down
+        for (int i = y; i < 0; i++) {
+            moves.add(new Move(myPosition,new Position(i,x)));
+        }
         return this.moves;
     }
 }
