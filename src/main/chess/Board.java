2559542717a4be9f81a3f23fb69253c8e94cd93a
Spencer Neu <stneu73@gmail.com>
2023-10-07 14:49:02 -0600

    Board passes both board tests

--- src/main/chess/Board.java
+++ src/main/chess/Board.java
@@ -18,30 +18,30 @@ public class Board implements ChessBoard {
         //black team
         ChessGame.TeamColor black = ChessGame.TeamColor.BLACK;
         for (int i = 0; i < 8; i++) {
-            addPiece(new Position(i,1), new Pawn(black));
+            addPiece(new Position(i,6,true), new Pawn(black));
             //piecesOnBoard[i][1] = new Pawn(black);
         }
-        piecesOnBoard[0][0] = new Rook(black);
-        piecesOnBoard[1][0] = new Knight(black);
-        piecesOnBoard[2][0] = new Bishop(black);
-        piecesOnBoard[3][0] = new Queen(black);
-        piecesOnBoard[4][0] = new King(black);
-        piecesOnBoard[5][0] = new Bishop(black);
-        piecesOnBoard[6][0] = new Knight(black);
-        piecesOnBoard[7][0] = new Rook(black);
+        addPiece(new Position(0,7,true),new Rook(black));
+        addPiece(new Position(1,7,true),new Knight(black));
+        addPiece(new Position(2,7,true),new Bishop(black));
+        addPiece(new Position(3,7,true),new Queen(black));
+        addPiece(new Position(4,7,true),new King(black));
+        addPiece(new Position(5,7,true),new Bishop(black));
+        addPiece(new Position(6,7,true),new Knight(black));
+        addPiece(new Position(7,7,true),new Rook(black));
         //white team
         ChessGame.TeamColor white = ChessGame.TeamColor.WHITE;
         for (int i = 0; i < 8; i++) {
-            piecesOnBoard[i][6] = new Pawn(white);
+            addPiece(new Position(i,1,true), new Pawn(white));
         }
-        piecesOnBoard[0][7] = new Rook(white);
-        piecesOnBoard[1][7] = new Knight(white);
-        piecesOnBoard[2][7] = new Bishop(white);
-        piecesOnBoard[3][7] = new Queen(white);
-        piecesOnBoard[4][7] = new King(white);
-        piecesOnBoard[5][7] = new Bishop(white);
-        piecesOnBoard[6][7] = new Knight(white);
-        piecesOnBoard[7][7] = new Rook(white);
+        addPiece(new Position(0,0,true),new Rook(white));
+        addPiece(new Position(1,0,true),new Knight(white));
+        addPiece(new Position(2,0,true),new Bishop(white));
+        addPiece(new Position(3,0,true),new Queen(white));
+        addPiece(new Position(4,0,true),new King(white));
+        addPiece(new Position(5,0,true),new Bishop(white));
+        addPiece(new Position(6,0,true),new Knight(white));
+        addPiece(new Position(7,0,true),new Rook(white));
         //null for all empty spaces
         for (int i = 2; i < 5;i++){
             for (int j = 0; j < 8; j++) {
