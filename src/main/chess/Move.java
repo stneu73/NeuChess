c9ee2228888a0903b828593b3a5944d6155924e2
Spencer Neu <stneu73@gmail.com>
2023-10-07 03:46:55 -0600

    Had IntelliJ generate equals() and hashCode() methods in Move and Position

--- src/main/chess/Move.java
+++ src/main/chess/Move.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 public class Move implements ChessMove{
     ChessPosition startPosition;
     ChessPosition endPosition;
@@ -26,5 +28,18 @@ public class Move implements ChessMove{
     @Override
     public ChessPiece.PieceType getPromotionPiece() {
         return promotionPiece;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Move move = (Move) o;
+        return Objects.equals(startPosition, move.startPosition) && Objects.equals(endPosition, move.endPosition) && promotionPiece == move.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
     }
 }
--- src/main/chess/Position.java
+++ src/main/chess/Position.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 public class Position implements ChessPosition{
     int row;
     int column;
@@ -28,5 +30,18 @@ public class Position implements ChessPosition{
     }
     public int getColumnIndex() {
         return column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return row == position.row && column == position.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
     }
 }
