239a7c200c2939465f4467496ca81c989cb0111c
Spencer Neu <stneu73@gmail.com>
2023-10-07 02:01:32 -0600

    Created Queen movement logic

--- src/main/chess/Queen.java
+++ src/main/chess/Queen.java
@@ -20,6 +20,190 @@ public class Queen implements ChessPiece{
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        int x = myPosition.getColumnIndex();
+        int y = myPosition.getRowIndex();
+        //Bishop Logic
+        if (y < 7) {
+            //NorthWest
+            if (x < 7) {
+                int yT = y;
+                int xT = x;
+                for (int i = 0; i < 8; i++, xT--,yT++) {
+                    if (xT < 0 || yT > 7) break;
+                    ChessPosition currPosition = new Position(xT,yT,true);
+                    if (board.getPiece(currPosition) != this) {
+                        if (board.getPiece(currPosition) == null) {
+                            moves.add(new Move(myPosition, currPosition));
+                        } else {
+                            ChessPiece piece = board.getPiece(currPosition);
+                            if (piece.getTeamColor() == this.color) {
+                                break;
+                            }
+                            if (piece.getTeamColor() != this.color) {
+                                moves.add(new Move(myPosition, currPosition));
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            //NorthEast
+            if (x > 0) {
+                int yT = y;
+                int xT = x;
+                for (int i = 0; i < 8; i++, xT++,yT++) {
+                    if (xT > 7 || yT > 7) break;
+                    ChessPosition currPosition = new Position(xT,yT,true);
+                    if (board.getPiece(currPosition) != this) {
+                        if (board.getPiece(currPosition) == null) {
+                            moves.add(new Move(myPosition, currPosition));
+                        } else {
+                            ChessPiece piece = board.getPiece(currPosition);
+                            if (piece.getTeamColor() == this.color) {
+                                break;
+                            }
+                            if (piece.getTeamColor() != this.color) {
+                                moves.add(new Move(myPosition, currPosition));
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (y > 0) {
+            //SouthEast
+            if (x < 7) {
+                int yT = y;
+                int xT = x;
+                for (int i = 0; i < 8; i++, xT++,yT--) {
+                    if (xT > 7 || yT < 0) break;
+                    ChessPosition currPosition = new Position(xT,yT,true);
+                    if (board.getPiece(currPosition) != this) {
+                        if (board.getPiece(currPosition) == null) {
+                            moves.add(new Move(myPosition, currPosition));
+                        } else {
+                            ChessPiece piece = board.getPiece(currPosition);
+                            if (piece.getTeamColor() == this.color) {
+                                break;
+                            }
+                            if (piece.getTeamColor() != this.color) {
+                                moves.add(new Move(myPosition, currPosition));
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            //SouthWest
+            if (x > 0 ) {
+                int yT = y;
+                int xT = x;
+                for (int i = 0; i < 8; i++, xT--,yT--) {
+                    if (xT < 0 || yT < 0) break;
+                    ChessPosition currPosition = new Position(xT,yT,true);
+                    if (board.getPiece(currPosition) != this) {
+                        if (board.getPiece(currPosition) == null) {
+                            moves.add(new Move(myPosition, currPosition));
+                        } else {
+                            ChessPiece piece = board.getPiece(currPosition);
+                            if (piece.getTeamColor() == this.color) {
+                                break;
+                            }
+                            if (piece.getTeamColor() != this.color) {
+                                moves.add(new Move(myPosition, currPosition));
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        //left
+        if (x > 0) {
+            for (int i = x; i >= 0; i--) {
+                ChessPosition currPosition = new Position(i,y,true);
+                if (board.getPiece(currPosition) == null) {
+                    moves.add(new Move(myPosition, currPosition));
+                }
+                if (i == 0) break;
+                currPosition = new Position(i - 1,y,true);
+                if (board.getPiece(currPosition) != null) {
+                    ChessPiece piece = board.getPiece(currPosition);
+                    if (piece.getTeamColor() == this.color) {
+                        break;
+                    }
+                    if (piece.getTeamColor() != this.color) {
+                        moves.add(new Move(myPosition, currPosition));
+                        break;
+                    }
+                }
+            }
+        }
+        //Rook Logic
+        //right
+        if (x < 7) {
+            for (int i = x; i < 8; i++) {
+                ChessPosition currPosition = new Position(i,y,true);
+                if (board.getPiece(currPosition) == null) {
+                    moves.add(new Move(myPosition, currPosition));
+                }
+                if (i == 7) break;
+                currPosition = new Position(i+1,y,true);
+                if (board.getPiece(currPosition) != null) {
+                    ChessPiece piece = board.getPiece(currPosition);
+                    if (piece.getTeamColor() == this.color) {
+                        break;
+                    }
+                    if (piece.getTeamColor() != this.color) {
+                        moves.add(new Move(myPosition, currPosition));
+                        break;
+                    }
+                }
+            }
+        }
+        //up
+        if (y < 7) {
+            for (int i = y; i < 8; i++) {
+                ChessPosition currPosition = new Position(x,i,true);
+                if (board.getPiece(currPosition) == null) {
+                    moves.add(new Move(myPosition, currPosition));
+                }
+                if (i == 7) break;
+                currPosition = new Position(x,i+1,true);
+                if (board.getPiece(currPosition) != null) {
+                    ChessPiece piece = board.getPiece(currPosition);
+                    if (piece.getTeamColor() == this.color) {
+                        break;
+                    }
+                    if (piece.getTeamColor() != this.color) {
+                        moves.add(new Move(myPosition,currPosition));
+                        break;
+                    }
+                }
+            }
+        }
+        //down
+        if (y > 0) {
+            for (int i = y; i >= 0; i--) {
+                ChessPosition currPosition = new Position(x,i,true);
+                if (board.getPiece(currPosition) == null) {
+                    moves.add(new Move(myPosition, currPosition));
+                }
+                if (i == 0) break;
+                currPosition = new Position(x,i-1,true);
+                if (board.getPiece(currPosition) != null) {
+                    ChessPiece piece = board.getPiece(currPosition);
+                    if (piece.getTeamColor() == this.color) {
+                        break;
+                    }
+                    if (piece.getTeamColor() != this.color) {
+                        moves.add(new Move(myPosition, currPosition));
+                        break;
+                    }
+                }
+            }
+        }
         return this.moves;
     }
 }
