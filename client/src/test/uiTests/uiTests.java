1c3ae5658a0c561191583b0f6c01605f81115ffa
Spencer Neu <stneu73@gmail.com>
2023-11-28 13:00:16 -0700

    Created uiTests

---
+++ client/src/test/uiTests/uiTests.java
@@ -0,0 +1,88 @@
+package uiTests;
+
+import org.junit.jupiter.api.*;
+import ui.PreLogin;
+
+public class uiTests {
+
+    @Test
+    public void loginPositive() {
+
+    }
+
+    @Test
+    public void loginNegative() {
+
+    }
+
+    @Test
+    public void quitPositive() {
+
+    }
+
+    @Test
+    public void quitNegative() {
+
+    }
+
+    @Test
+    public void registerPositive() {
+
+    }
+
+    @Test
+    public void registerNegative() {
+
+    }
+
+    @Test
+    public void logoutPositive() {
+
+    }
+
+    @Test
+    public void logoutNegative() {
+
+    }
+
+    @Test
+    public void createGamePositive() {
+
+    }
+
+    @Test
+    public void createGameNegative() {
+
+    }
+
+    @Test
+    public void listGamesPositive() {
+
+    }
+
+    @Test
+    public void listGamesNegative() {
+
+    }
+
+    @Test
+    public void joinGamePositive() {
+
+    }
+
+    @Test
+    public void joinGameNegative() {
+
+    }
+
+    @Test
+    public void joinObserverPositive() {
+
+    }
+
+    @Test
+    public void joinObserverNegative() {
+
+    }
+
+}
